b01829e969f507fe7d6da4499d57d1b68308e011
caishuo <caishuo@metanas.com>
2024-08-04 21:24:09 +0800

    指定打包到docker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,34 @@
 export default defineNuxtConfig({
-  devServer: {
-    port: 9527,
-  },
-  // 全局模块引入
-  modules: [
-    '@vueuse/nuxt',
-    '@unocss/nuxt',
-    '@pinia/nuxt',
-    '@nuxtjs/color-mode',
-    '@element-plus/nuxt',
-  ],
-  experimental: {
-    reactivityTransform: true,
-    inlineSSRStyles: false,
-  },
-  // 全局css引入
-  css: [
-    '@unocss/reset/tailwind.css',
-    '@/assets/scss/index.scss',
-    '@/styles/index.scss',
-    '@/styles/anime.css',
-  ],
-  colorMode: {
-    classSuffix: '',
-  },
+    devServer: {
+        port: 9527,
+    },
+    // 全局模块引入
+    modules: [
+        '@vueuse/nuxt',
+        '@unocss/nuxt',
+        '@pinia/nuxt',
+        '@nuxtjs/color-mode',
+        '@element-plus/nuxt',
+    ],
+    experimental: {
+        reactivityTransform: true,
+        inlineSSRStyles: false,
+    },
+    // 全局css引入
+    css: [
+        '@unocss/reset/tailwind.css',
+        '@/assets/scss/index.scss',
+        '@/styles/index.scss',
+        '@/styles/anime.css',
+    ],
+    colorMode: {
+        classSuffix: '',
+    },
+    nitro: {
+        output: {
+            publicDir: "../docker/dist/public",
+            serverDir: "../docker/dist/server",
+            dir:"../docker/dist",
+        }
+    }
 })
