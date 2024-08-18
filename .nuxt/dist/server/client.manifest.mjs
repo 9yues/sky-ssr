6fede54fe10b08830b2c5375ad203a1f2df196d7
VisitorCS <35484545+VisitorCS@users.noreply.github.com>
2024-08-18 22:28:56 +0800

    Delete .nuxt directory

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/liuxiaolong/Documents/work/sky-ssr/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/liuxiaolong/Documents/work/sky-ssr/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
